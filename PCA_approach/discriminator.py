e617f55bbdb700a072d94680b745a5a4c92d7fc1
Moritz Nottebaum <moritz@nottebaum-online.de>
2020-06-10 19:36:44 +0200

    Discriminator done

--- PCA_approach/discriminator.py
+++ PCA_approach/discriminator.py
@@ -1,1 +1,185 @@
+import torchvision.transforms as transforms
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+
+
+class Discriminator(nn.Module):
+        def __init__(self, ngpu):
+            super(Discriminator, self).__init__()
+            self.ngpu = ngpu
+            self.main = nn.Sequential(
+                # input is (nc) x 64 x 64
+                nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+                nn.LeakyReLU(0.2, inplace=True),
+                # state size. (ndf) x 32 x 32
+                nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(ndf * 2),
+                nn.LeakyReLU(0.2, inplace=True),
+                # state size. (ndf*2) x 16 x 16
+                nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(ndf * 4),
+                nn.LeakyReLU(0.2, inplace=True),
+                # state size. (ndf*4) x 8 x 8
+                nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+                nn.BatchNorm2d(ndf * 8),
+                nn.LeakyReLU(0.2, inplace=True),
+                # state size. (ndf*8) x 4 x 4
+                nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+                nn.Sigmoid()
+            )
+
+        def forward(self, input):
+            return self.main(input)
+
+if __name__ == '__main__':   
+    workers = 1
+
+    # Batch size during training
+    batch_size = 16
+
+    # Spatial size of training images. All images will be resized to this
+    #   size using a transformer.
+    image_size = 64
+
+    # Number of channels in the training images. For color images this is 3
+    nc = 3
+
+    # Size of z latent vector (i.e. size of generator input)
+    nz = 100
+
+    # Size of feature maps in generator
+    ngf = 64
+
+    # Size of feature maps in discriminator
+    ndf = 64
+
+    # Number of training epochs
+    num_epochs = 20
+
+    # Learning rate for optimizers
+    lr = 0.0002
+
+    # Beta1 hyperparam for Adam optimizers
+    beta1 = 0.5
+
+    # Number of GPUs available. Use 0 for CPU mode.
+    ngpu = 1
+
+
+
+
+    def weights_init(m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
+
+
+
+
+
+    dataroot = os.getcwd() + "/../emojis_jpg_root/"
+
+    device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+    dataset = dset.ImageFolder(root=dataroot,
+                            transform=transforms.Compose([
+                                transforms.Resize(image_size),
+                                transforms.CenterCrop(image_size),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                            ]))
+    # Create the dataloader
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                            shuffle=True, num_workers=workers)
+
+
+
+
+
+    
+
+
+
+    netD = Discriminator(ngpu).to(device)
+
+    # Handle multi-gpu if desired
+    if (device.type == 'cuda') and (ngpu > 1):
+        netD = nn.DataParallel(netD, list(range(ngpu)))
+
+    # Apply the weights_init function to randomly initialize all weights
+    #  to mean=0, stdev=0.2.
+    netD.apply(weights_init)
+
+
+    criterion = nn.BCELoss()
+    optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+
+    real_label = 1
+    fake_label = 0
+
+    D_losses = []
+
+    for epoch in range(num_epochs):
+        # For each batch in the dataloader
+        for i, data in enumerate(dataloader, 0):
+
+            ############################
+            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+            ###########################
+            ## Train with all-real batch
+            netD.zero_grad()
+            # Format batch
+            real_cpu = data[0].to(device)
+            b_size = real_cpu.size(0)
+            label = torch.full((b_size,), real_label, device=device)
+            # Forward pass real batch through D
+            output = netD(real_cpu).view(-1)
+            # Calculate loss on all-real batch
+            errD_real = criterion(output, label)
+            # Calculate gradients for D in backward pass
+            errD_real.backward()
+            D_x = output.mean().item()
+
+            ## Train with all-fake batch
+            
+            noise = torch.randn(b_size, nz, 1, 1, device=device)
+            
+            
+            # Here comes the PCA output at the moment its just noise
+            #fake = netG(noise)
+            fake = torch.randn(b_size, 3, 64, 64, device=device)
+            label.fill_(fake_label)
+            
+            # Classify all fake batch with D
+            output = netD(fake.detach()).view(-1)
+            # Calculate D's loss on the all-fake batch
+            errD_fake = criterion(output, label)
+            # Calculate the gradients for this batch
+            errD_fake.backward()
+            D_G_z1 = output.mean().item()
+            # Add the gradients from the all-real and all-fake batches
+            errD = errD_real + errD_fake
+            # Update D
+            optimizerD.step()
+
+            D_losses.append(errD.item())
+
+
+    print(D_losses)
+    plt.plot(D_losses, label="D")
+    plt.show()
+
+
