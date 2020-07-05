249609c71b61e5863177ab1f3ed1367b6bc2bc94
kolev <mr_kolev95@gmx.de>
2020-07-05 15:41:01 +0200

    Adding inference on segment cgan, combining parts. Adding pics. TODO: Cleanup code and integrate with captions_cgan

--- cgan/combiner.py
+++ cgan/combiner.py
@@ -5,25 +5,13 @@ import numpy as np
 from cgan.dataloading import EmojiFaceSegments
 
 
-def query_cgan(model_path):
-    # Load model
+def combine_parts(parts_list, nogan=True):
+    dataset_path = "../segmentation/segmentation_data" if nogan else "inference_results"
 
-    # Query model for each part
-
-    # Create background
-
-    # Paste parts on background
-
-    # Save pic
-    pass
-
-
-def combine_nogan(parts_list):
     # Create background
     background = create_background()
 
     # Load dataset
-    dataset_path = "../segmentation/segmentation_data"
     dataset = EmojiFaceSegments(dataset_path)
 
     # Choose parts
@@ -33,12 +21,15 @@ def combine_nogan(parts_list):
         pick = random.choice(curr_parts).copy()
         #a = list(filter(lambda x: np.all(x), pick < 40))
         # TODO: More efficient impl. using map and filter
+        if not nogan:
+            pick *= 255
+        thresh = 30
         for row in range(pick.shape[0]):
             for col in range(pick.shape[1]):
-                if np.all(pick[row][col] < 30):
+                if np.all(pick[row][col] < thresh):
                     pick[row][col] = [0,0,0]
 
-        pick = Image.fromarray(pick)
+        pick = Image.fromarray((pick).astype(np.uint8))
         pick = pick.convert("RGBA")
         pixdata = pick.load()
 
@@ -64,9 +55,9 @@ def create_background():
     background = Image.new("RGB", (64, 64), (0,0,0))
     img = ImageDraw.Draw(background)
     img.ellipse((6, 6, 58, 58), fill=(254,215,50), outline=(220,134,11), width=1)
-    background.save("test.jpg", 'JPEG', quality=100)
+    #background.save("test.jpg", 'JPEG', quality=100)
     return background
 
 
 # labels = {"ears": 1, "eyebrows": 2, "eyes": 3, "hands": 4, "mouth": 5, "tears": 6}
-combine_nogan([1, 2, 3, 4, 5, 6])
+combine_parts([1, 2, 3, 4, 5, 6], nogan=False)
--- cgan/trainer.py
+++ cgan/trainer.py
@@ -1,6 +1,8 @@
 from __future__ import print_function, division
 
 import torch.nn as nn
+import torchvision
+
 from cgan.models import Discriminator, Generator
 from torch.utils.data import DataLoader
 from cgan.dataloading import load_captions_from_textfile
@@ -9,6 +11,7 @@ import torch.optim as optim
 from torchvision import utils as tv_ut
 from torch.utils.data import random_split
 import os
+import numpy as np
 from cgan.embeddings.glove_loader import GloveModel
 
 class CGanTrainer():
@@ -117,36 +120,53 @@ class CGanTrainer():
         torch.save(self.generator.state_dict(), gen_weights_path)
         torch.save(self.discriminator.state_dict(), dis_weights_path)
 
-    def generate_images(self, cond, output_path, x_in=None):
+    def generate_images(self, cond, output_path, x_in=None, grid=True):
         if x_in is None:
             x_in = torch.randn(len(cond), 1, self.latent_dim).to(self.device)
         cond = cond.to(self.device)
         self.generator.eval()
         with torch.no_grad():
             output = self.generator(x_in, cond)
-            save_image_batch(output.detach(), output_path)
-        self.generator.train()
+            save_image_batch(output.detach(), output_path, grid=grid)
+        #self.generator.train()
 
-    def inference(self, captions, output_path="inference_results.png", glove_model=None, x_in=None):
+    def inference(self, captions, output_path="inference_results/", glove_model=None, x_in=None):
         self.discriminator.to(self.device)
         self.generator.to(self.device)
         if type(captions) is list:
             captions = captions
         else:
             captions = load_captions_from_textfile(captions)
+        """
         if not glove_model:
             glove_model = GloveModel()
             glove_model.load("cgan/embeddings/glove.6B.300d.txt")
         captions = torch.Tensor([glove_model.encode_docs([c]) for c in captions])
-        self.generate_images(captions, output_path=output_path, x_in=x_in)
+        """
+        onehots = []
+        for label in captions:
+            onehot = np.zeros((1, len(captions)))
+            onehot[0, label - 1] = 1
+            onehots.append(onehot)
+        captions = torch.Tensor(onehots)
+        self.generate_images(captions, output_path=output_path, x_in=x_in, grid=False)
 
 
-def save_image_batch(image_batch, output_path):
+def save_image_batch(image_batch, output_path, grid=True):
     output_dir = os.path.dirname(output_path)
     if output_dir != '' and not os.path.exists(output_dir):
         os.mkdir(output_dir)
-    grid = tv_ut.make_grid(image_batch, normalize=True, padding=0)
-    tv_ut.save_image(grid, output_path)
+    if grid:
+        grid = tv_ut.make_grid(image_batch, normalize=True, padding=0)
+        tv_ut.save_image(grid, output_path)
+    else:
+        NAMES = ["ears/", "eyebrows/", "eyes/", "hands/", "mouth/", "tears/"]
+        for i in range(image_batch.size(0)):
+            name = NAMES[i]
+            name_dir = os.path.dirname(output_path+name)
+            if name_dir != '' and not os.path.exists(name_dir):
+                os.mkdir(name_dir)
+            torchvision.utils.save_image(image_batch[i, :, :, :], output_path+name+'{}.png'.format(i+1))
 
 
 def five():
